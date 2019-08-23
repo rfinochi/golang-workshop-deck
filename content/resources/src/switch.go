f857822d669af1faf2f6994b08d1252d68ec9643
Rodolfo Finochietti <rfinochi@gmail.com>
2019-08-23 10:48:11 -0300

    Exercise 7

--- content/resources/src/switch.go
+++ content/resources/src/switch.go
@@ -1,23 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	const finger = 3
-	// const finger = 6 // Should go to the default section
+	fmt.Print("Go runs on ")
 
-	switch finger {
-	case 1:
-		fmt.Println("Thumb")
-	case 2:
-		fmt.Println("Index")
-	case 3:
-		fmt.Println("Middle")
-	case 4:
-		fmt.Println("Ring")
-	case 5:
-		fmt.Println("Pinky")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
 	default:
-		fmt.Println("incorrect finger number")
+		fmt.Printf("%s.", os)
 	}
 }
