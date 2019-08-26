611bca196d4a1b2df5c5bd4a72aff8a308f53b76
Rodolfo Finochietti <rfinochi@gmail.com>
2019-08-26 10:41:26 -0300

    Minor changes

--- content/resources/src/types.go
+++ content/resources/src/types.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var i int16 = 55     //int16
-	var j float32 = 67.8 //float32
-	sum := i + int16(j)  //j is converted to int
+	var i int16 = 55     // int16
+	var j float32 = 67.8 // float32
+	sum := i + int16(j)  // j is converted to int
 	fmt.Println(sum)
 
 	const aa int = 48
@@ -16,7 +16,7 @@ func main() {
 	fmt.Println(aa, bb)
 
 	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y)) //Explicit type conversion
-	var z uint = uint(f) //Explicit type conversion
+	var f float64 = math.Sqrt(float64(x*x + y*y)) // Explicit type conversion
+	var z uint = uint(f) // Explicit type conversion
 	fmt.Println(x, y, z)
 }
